b2e7233d3596bfa01549d02e89c254ae7e28e663
Lincheng Gu(顾林成) <lincheng.gu@ragent.cn>
2016-03-15 19:10:09 +0800

    delete all
    Signed-off-by: Lincheng Gu(顾林成) <lincheng.gu@ragent.cn>

--- mtkgallery/src/com/mediatek/galleryfeature/mav/IconView.java
+++
@@ -1,7 +0,0 @@
-package com.mediatek.galleryfeature.mav;
-
-import com.mediatek.galleryframework.gl.MGLView;
-
-public interface IconView extends MGLView {
-    public void setVisibility(boolean isVisible);
-}
